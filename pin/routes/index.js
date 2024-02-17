00fa767ee8bac1287772f6a08a76211aafd7aeeb
vishu bhandari <deevishu1234@gmail.com>
2024-02-17 23:18:02 +0530

    registering features added

--- pin/routes/index.js
+++ pin/routes/index.js
@@ -1,9 +1,58 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const passport = require("passport");
 
-/* GET home page. */
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/register',function(req,res){
+  res.render('register');
+})
+
+router.get('/profile',isLoggedIn,function(req,res,next){
+  res.render("profile");
+})
+
+router.post('/register',function(req,res){
+  const data=new userModel({
+    username:req.body.username,
+    email:req.body.email,
+    contact:req.body.contact
+  })
+  userModel.register(data,req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile");
+    })
+  })
+})
+
+router.post('/login',passport.authenticate("local",{
+  failureRedirect:"/",
+  successRedirect:"/profile",
+}),function(req,res,next){
+
+});
+
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
+
 
 module.exports = router;
--- pin/routes/users.js
+++ pin/routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const mongoose=require('mongoose')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const plm=require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/pin");
+
+
+
+
+
+const userSchema=mongoose.Schema({
+  username:String,
+  name:String,
+  email:String,
+  password:String,
+  profileImage:String,
+  contact:Number,
+  boards:{
+    type:Array,
+    default:[]
+  },
 });
 
-module.exports = router;
+userSchema.plugin(plm);
+module.exports=mongoose.model("user",userSchema);
