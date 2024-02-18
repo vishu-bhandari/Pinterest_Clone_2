7ec20eb83ee61f6f163671683e0487fc625a1aab
vishu bhandari <deevishu1234@gmail.com>
2024-02-18 15:08:58 +0530

    profile added

--- pin/routes/index.js
+++ pin/routes/index.js
@@ -4,41 +4,62 @@ const userModel = require("./users");
 const passport = require("passport");
 
 const localStrategy = require("passport-local");
+const upload = require("./multer");
 passport.use(new localStrategy(userModel.authenticate()));
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
 
-router.get('/register',function(req,res){
-  res.render('register');
-})
+router.get("/register", function (req, res) {
+  res.render("register");
+});
 
-router.get('/profile',isLoggedIn,function(req,res,next){
-  res.render("profile");
-})
+router.get("/profile", isLoggedIn,async function (req, res, next) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user,
+  });
+  res.render("profile",{user});
+});
 
-router.post('/register',function(req,res){
-  const data=new userModel({
-    username:req.body.username,
-    email:req.body.email,
-    contact:req.body.contact
-  })
-  userModel.register(data,req.body.password)
-  .then(function(){
-    passport.authenticate("local")(req,res,function(){
+router.post(
+  "/fileupload",
+  isLoggedIn,
+  upload.single("image"),
+  async function (req, res) {
+    if (!req.file) {
+      return res.status(404).send("no files were given");
+    }
+    const user = await userModel.findOne({
+      username: req.session.passport.user,
+    });
+    user.profileImage = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+  }
+);
+
+router.post("/register", function (req, res) {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact,
+  });
+  userModel.register(data, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
       res.redirect("/profile");
-    })
-  })
-})
-
-router.post('/login',passport.authenticate("local",{
-  failureRedirect:"/",
-  successRedirect:"/profile",
-}),function(req,res,next){
-
+    });
+  });
 });
 
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    successRedirect: "/profile",
+  }),
+  function (req, res, next) {}
+);
 
 router.get("/logout", function (req, res) {
   req.logout(function (err) {
@@ -53,6 +74,5 @@ function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect("/");
 }
-
 
 module.exports = router;
