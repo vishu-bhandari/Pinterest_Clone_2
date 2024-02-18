4efd97cfe2ab0d961141b66fe359323c9e3708f9
vishu bhandari <deevishu1234@gmail.com>
2024-02-18 18:01:01 +0530

    post route added and saved in db

--- pin/routes/index.js
+++ pin/routes/index.js
@@ -2,24 +2,44 @@ var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
 const passport = require("passport");
-
+const postModel=require('./post')
 const localStrategy = require("passport-local");
 const upload = require("./multer");
 passport.use(new localStrategy(userModel.authenticate()));
 
 router.get("/", function (req, res, next) {
-  res.render("index");
+  res.render("index" , {nav:false});
 });
 
 router.get("/register", function (req, res) {
-  res.render("register");
+  res.render("register",{nav:false});
 });
+
+router.get("/add",isLoggedIn,async function(req,res){
+  const user=await userModel.findOne({username:req.session.passport.user});
+  res.render("add", {nav:true});
+})
+router.post("/createpost",isLoggedIn,upload.single("postimage"),async function(req,res){
+  const user=await userModel.findOne({username:req.session.passport.user});
+  const post =await postModel.create({
+    user:user._id,
+    title:req.body.title,
+    description:req.body.description,
+    image:req.file.filename
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+})
+
+
 
 router.get("/profile", isLoggedIn,async function (req, res, next) {
   const user = await userModel.findOne({
     username: req.session.passport.user,
   });
-  res.render("profile",{user});
+  res.render("profile",{user , nav:true});
+  
 });
 
 router.post(
--- pin/routes/users.js
+++ pin/routes/users.js
@@ -1,25 +1,27 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const plm=require("passport-local-mongoose");
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/pin");
 
-
-
-
-
-const userSchema=mongoose.Schema({
-  username:String,
-  name:String,
-  email:String,
-  password:String,
-  profileImage:String,
-  contact:Number,
-  boards:{
-    type:Array,
-    default:[]
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default: [],
   },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
 });
 
 userSchema.plugin(plm);
-module.exports=mongoose.model("user",userSchema);
+module.exports = mongoose.model("user", userSchema);
